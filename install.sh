71a8b7167741640d7b55c25d830faed129a4371d
Mohammed Ajil <mohammed@ajil.ch>
2019-02-12 17:37:06 +0100

    completed installation script draft

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 current_dir=$(pwd)
 bash_config_path=$current_dir/bash_prompt.sh
 echo "source $bash_config_path" >> ~/.bashrc
-echo "alias jn=\"jupyter notebook\""
+echo "alias jn=\"jupyter notebook\"" >> ~/.bashrc
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 wget https://bootstrap.pypa.io/get_pip.py
 python3 get_pip.py
@@ -14,4 +15,22 @@ sudo jupyter nbextensions_configurator enable
 sudo jupyter nbextension enable hinterland/hinterland
 sudo jupyter nbextension enable codefolding/main
 
-sudo apt install git-core gcloud kubectl remmina python3-dev
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
+ln -sf /opt/kubectx/completion/kubens.bash $COMPDIR/kubens
+ln -sf /opt/kubectx/completion/kubectx.bash $COMPDIR/kubectx
+echo "export PATH=/opt/kubectx:\$PATH" >> ~/.bashrc
+
+CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt update
+sudo apt install git-core google-cloud-sdk kubectl remmina python3-dev code guake 
