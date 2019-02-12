d28c536543260d5e1d6442bf4546bb0d253e17f0
Mohammed Ajil <mohammed@ajil.ch>
2019-02-12 15:11:33 +0100

    started installation script

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@ current_dir=$(pwd)
 bash_config_path=$current_dir/bash_prompt.sh
 echo "source $bash_config_path" >> ~/.bashrc
 
+wget https://bootstrap.pypa.io/get_pip.py
+python3 get_pip.py
+
+sudo -H pip3 install pipenv jupyter jupyter_contrib_nbextensions jupyter_nbextensions_configurator
+
+sudo jupyter contrib nbextension install
+sudo jupyter nbextensions_configurator enable
+sudo jupyter nbextension enable hinterland/hinterland
+sudo jupyter nbextension enable codefolding/main
+
+sudo apt install git-core gcloud kubectl remmina python3-dev
