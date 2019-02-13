efe415c4b7e6383927f851eaeac364d64d5a2111
Mohammed Ajil <mohammed@ajil.ch>
2019-02-13 09:27:58 +0100

    finished install script

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ echo "source $bash_config_path" >> ~/.bashrc
 echo "alias jn=\"jupyter notebook\"" >> ~/.bashrc
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
-wget https://bootstrap.pypa.io/get_pip.py
-python3 get_pip.py
+sudo apt update
+sudo apt install -y git-core python3-dev
+
+wget https://bootstrap.pypa.io/get-pip.py -O ./get-pip.py
+python3 ./get-pip.py
 
 sudo -H pip3 install pipenv jupyter jupyter_contrib_nbextensions jupyter_nbextensions_configurator
 
@@ -28,9 +31,12 @@ ln -sf /opt/kubectx/completion/kubens.bash $COMPDIR/kubens
 ln -sf /opt/kubectx/completion/kubectx.bash $COMPDIR/kubectx
 echo "export PATH=/opt/kubectx:\$PATH" >> ~/.bashrc
 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt update
-sudo apt install git-core google-cloud-sdk kubectl remmina python3-dev code guake 
+sudo apt install -y google-cloud-sdk remmina code guake kubectl
