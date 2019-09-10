89fdb3b1ca354e816b9ff4267863a6907ab5271f
Mohammed Ajil <muy@x1.localdomain>
2019-09-10 23:13:53 +0200

    update install script for pop os

--- install.sh
+++ install.sh
@@ -8,24 +8,23 @@ echo "alias jl=\"jupyter-lab\"" >> ~/.bashrc
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 sudo apt update
-sudo apt install -y git-core python3-dev
+sudo apt remove libreoffice* firefox
+sudo apt autoremove
+sudo apt install -y git-core python3-pip code slack-desktop docker.io docker-compose google-chrome-stable guake htop vlc pkgconf
 
-wget https://bootstrap.pypa.io/get-pip.py -O ./get-pip.py
-python3 ./get-pip.py
-
+sudo -H pip3 install --upgrade pip
 sudo -H pip3 install pipenv jupyterlab
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo wget -O /usr/local/bin/latexdocker https://raw.githubusercontent.com/blang/latex-docker/master/latexdockercmd.sh
+sudo chmod +x /usr/local/bin/latexdocker
 
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
 COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-ln -sf /opt/kubectx/completion/kubens.bash $COMPDIR/kubens
-ln -sf /opt/kubectx/completion/kubectx.bash $COMPDIR/kubectx
+sudo ln -sf /opt/kubectx/completion/kubens.bash $COMPDIR/kubens
+sudo ln -sf /opt/kubectx/completion/kubectx.bash $COMPDIR/kubectx
 echo "export PATH=/opt/kubectx:\$PATH" >> ~/.bashrc
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -36,11 +35,7 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt update
-sudo apt install -y google-cloud-sdk remmina code guake kubectl htop
+sudo apt install -y google-cloud-sdk kubectl
 
 rm ~/.config/Code/User/settings.json
 ln -s $current_dir/vs-code-settings.json ~/.config/Code/User/settings.json
-
-# TODO: Install VLC
-# TODO: Install pdflatex
-# TODO: Install numix icon pack
