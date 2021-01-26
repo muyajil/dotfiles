ef96951edceb3bedbeb1dc754fec26f0470dd376
Mohammed <mohammed@ajil.ch>
2021-01-26 10:20:29 +0100

    better uninstall technique

--- install.sh
+++ install.sh
@@ -24,8 +24,15 @@ read -p "Press enter to execute next step"
 
 # Uninstall unnecessary things
 echo "Uninstall unneeded packages..."
-sudo apt update
-sudo apt remove libreoffice* firefox docker docker-engine docker.io containerd runc
+pkgToRemoveListFull="libreoffice-base libreoffice-base-core libreoffice-calc libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-help-en-us libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-writer python3-uno firefox docker docker-engine docker.io containerd runc"
+pkgToRemoveList=""
+for pkgToRemove in $(echo $pkgToRemoveListFull); do
+  $(dpkg --status $pkgToRemove &> /dev/null)
+  if [[ $? -eq 0 ]]; then
+    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
+  fi
+done
+apt-get --yes --purge remove $pkgToRemoveList
 sudo apt autoremove
 read -p "Press enter to execute next step"
 
