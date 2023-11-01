c227c58e175012f3232d2093bd579884320ca052
Mohammed Ajil <mohammed.ajil@quantco.com>
2023-11-01 11:34:06 +0100

    remove shell stuff because of warp

--- install-mac.sh
+++ install-mac.sh
@@ -9,7 +9,7 @@ sudo cp $current_dir/c.sh /usr/local/bin/c
 sudo cp $current_dir/conda-pip-install.sh /usr/local/bin/conda-pip-install
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 rm -f ~/.zshrc
 
@@ -29,23 +29,23 @@ echo "source $paths_path" >> ~/.zshrc
 echo "source $env_vars_path" >> ~/.zshrc
 echo "source $brew_completions" >> ~/.zshrc
 
-echo 'export ZSH="$HOME/.oh-my-zsh"' >> ~/.zshrc
-echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
+# echo 'export ZSH="$HOME/.oh-my-zsh"' >> ~/.zshrc
+# echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
 
 # Install plugins and themes for zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-curl -fsSL -o $HOME/.oh-my-zsh/custom/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
-curl -fsSL -o $HOME/Downloads/Tomorrow\ Night\ Eighties.terminal https://github.com/chriskempson/tomorrow-theme/raw/master/OS%20X%20Terminal/Tomorrow%20Night%20Eighties.terminal
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# curl -fsSL -o $HOME/.oh-my-zsh/custom/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+# curl -fsSL -o $HOME/Downloads/Tomorrow\ Night\ Eighties.terminal https://github.com/chriskempson/tomorrow-theme/raw/master/OS%20X%20Terminal/Tomorrow%20Night%20Eighties.terminal
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Install fonts
-git clone https://github.com/powerline/fonts.git $HOME/repositories/fonts
-mkdir $HOME/repositories/fonts/MesloLGS
-curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-$HOME/repositories/fonts/install.sh
+# git clone https://github.com/powerline/fonts.git $HOME/repositories/fonts
+# mkdir $HOME/repositories/fonts/MesloLGS
+# curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+# curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+# curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+# curl -fsSL -o $HOME/repositories/fonts/MesloLGS/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+# $HOME/repositories/fonts/install.sh
 
 # Install homebrew
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -66,15 +66,16 @@ brew install \
     threema \
     plex \
     yt-dlp \
-    gh
+    gh \
+    warp
 
 # Needed for dcps.py
 conda install tabulate
 sudo xattr -d com.apple.quarantine /usr/local/bin/dcps
 
-# Conda config
-conda init zsh
-conda config --set changeps1 False
+# # Conda config
+# conda init zsh
+# conda config --set changeps1 False
 
 # Configs
 current_dir=$(pwd)
@@ -84,4 +85,4 @@ ln -s $current_dir/vs-code-settings.json $HOME/Library/Application\ Support/Code
 mkdir -p $HOME/.ssh
 ln -s $current_dir/ssh_config ~/.ssh/config
 
-echo "You need to import the color palette in the downloads folder and set it as default for terminal and choose a powerline font as well"
+# echo "You need to import the color palette in the downloads folder and set it as default for terminal and choose a powerline font as well"
